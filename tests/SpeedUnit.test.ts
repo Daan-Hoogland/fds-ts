45500e6f02820ac9026d0c873a4e3471476bc6a4
Daan Hoogland <daan@hoogland.io>
2025-02-19 20:42:05 +0100

    Fix ESpeedUnit test

--- tests/SpeedUnit.test.ts
+++ tests/SpeedUnit.test.ts
@@ -1,23 +1,33 @@
 import { SpeedUnit, getSpeedUnit } from '../src/ESpeedUnit'
 
 describe('SpeedUnit enum', () => {
-    it('should return the right enum value for the input string options', () => {
+    it('should return the right enum value for the input string MH', () => {
         const meterString = 'MH'
-        const feetString = 'FPH'
-        const kmString = 'KPH'
-        const mString = 'MPH'
-        const msString = 'MS'
-        const fpsString = 'FPS'
         expect(getSpeedUnit(meterString)).toBe(SpeedUnit.MH)
+    })
+    it('should return the right enum value for the input string FPH', () => {
+        const feetString = 'FPH'
         expect(getSpeedUnit(feetString)).toBe(SpeedUnit.FPH)
+    })
+    it('should return the right enum value for the input string KPH', () => {
+        const kmString = 'KPH'
         expect(getSpeedUnit(kmString)).toBe(SpeedUnit.KPH)
+    })
+    it('should return the right enum value for the input string MPH', () => {
+        const mString = 'MPH'
         expect(getSpeedUnit(mString)).toBe(SpeedUnit.MPH)
+    })
+    it('should return the right enum value for the input string MS', () => {
+        const msString = 'MS'
         expect(getSpeedUnit(msString)).toBe(SpeedUnit.MS)
+    })
+    it('should return the right enum value for the input string FPS', () => {
+        const fpsString = 'FPS'
         expect(getSpeedUnit(fpsString)).toBe(SpeedUnit.FPS)
     })
-    it('should correctly return UNKNOWN when fed an invalid distance value', () => {
+    it('should correctly return UNKNOWN when fed an invalid speed value', () => {
         const inputString = 'INVALID VALUE'
-        const distanceEnum = getSpeedUnit(inputString)
-        expect(distanceEnum).toBe(SpeedUnit.UNKNOWN)
+        const speedEnum = getSpeedUnit(inputString)
+        expect(speedEnum).toBe(SpeedUnit.UNKNOWN)
     })
 })
