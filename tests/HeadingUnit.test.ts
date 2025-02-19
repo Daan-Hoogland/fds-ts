cd1254cdb14025547002f021d0f071b39c7b601f
Daan Hoogland <daan@hoogland.io>
2025-02-19 20:40:25 +0100

    Rename EHeadingUnit file & add tests

---
+++ tests/HeadingUnit.test.ts
@@ -0,0 +1,21 @@
+import { HeadingUnit, getHeadingUnit } from '../src/EHeadingUnit'
+
+describe('SpeedUnit enum', () => {
+    it('should return the right enum value for the input string RADIAN', () => {
+        const radianString = 'RADIAN'
+        expect(getHeadingUnit(radianString)).toBe(HeadingUnit.RADIAN)
+    })
+    it('should return the right enum value for the input string HEADING_TRUE', () => {
+        const headingTrueString = 'HEADING_TRUE'
+        expect(getHeadingUnit(headingTrueString)).toBe(HeadingUnit.HEADING_TRUE)
+    })
+    it('should return the right enum value for the input string DEGREE', () => {
+        const degreeString = 'DEGREE'
+        expect(getHeadingUnit(degreeString)).toBe(HeadingUnit.DEGREE)
+    })
+    it('should correctly return UNKNOWN when fed an invalid heading value', () => {
+        const inputString = 'INVALID VALUE'
+        const headingEnum = getHeadingUnit(inputString)
+        expect(headingEnum).toBe(HeadingUnit.UNKNOWN)
+    })
+})
